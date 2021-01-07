d0dee9b48bfae1ee22b4e824d28b64ab1e0395a9
Dalvin Segura <bydalvin4k@gmail.com>
2021-01-07 19:15:58 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// $(document).ready(function(){
+
+    var navBtn = $('.styles-nav-btn');
+    var navBtnSpan = $('#nav-btn-span');
+
+    let test1 = $('.styles-nav-btn');
+
+
+navBtn.on('mouseover', function(){
+    $(this).toggle().css({
+        opacity: 0
+    })
+    // console.log('funcionó el hover')
+});
+
+
+
+// document.body.style.background = '#000'
+
+
+
+
+// });
