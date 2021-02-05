cb3b92f45c22915ec23ba0ddad054ade01f013ae
Dalvin Segura <bydalvin4k@gmail.com>
2021-02-05 12:43:18 -0400

    pagina mejorada, se le agrego mas contenido pero aun falta por mejorar

--- js/main.js
+++ js/main.js
@@ -1,21 +1,6 @@
 // $(document).ready(function(){
 
-    var navBtn = $('.styles-nav-btn');
-    var navBtnSpan = $('#nav-btn-span');
 
-    let test1 = $('.styles-nav-btn');
-
-
-navBtn.on('mouseover', function(){
-    $(this).toggle().css({
-        opacity: 0
-    })
-    // console.log('funcionó el hover')
-});
-
-
-
-// document.body.style.background = '#000'
 
 
 
